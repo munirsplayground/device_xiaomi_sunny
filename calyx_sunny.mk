576444832f090f772b184cf8ff5bb7f9d8b89675
mnasibzade <euoryexe@gmail.com>
2024-09-28 18:04:59 +0400

    sunny: Calyx-ify

--- calyx_sunny.mk
+++ calyx_sunny.mk
@@ -11,25 +11,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sunny device
 $(call inherit-product, device/xiaomi/sunny/device.mk)
 
-# Inherit some common AlphaDroid stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
-# Device config
-TARGET_HAS_UDFPS := false
-TARGET_ENABLE_BLUR := false
-TARGET_EXCLUDES_AUDIOFX := true
-TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_BOOT_ANIMATION_RES := 1080
-
-# Debugging
-TARGET_INCLUDE_MATLOG := false
-
-# Maintainer
-ALPHA_BUILD_TYPE := Official
-ALPHA_MAINTAINER := eklerismunir
+# Inherit some common CalyxOS stuff.
+$(call inherit-product, vendor/calyx/config/common.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_sunny
+PRODUCT_NAME := calyx_sunny
 PRODUCT_DEVICE := sunny
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := M2101K7AG
